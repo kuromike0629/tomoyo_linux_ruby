e844cae28af70731e84c87f7b3fbef807de0f4cd
kuromike0629 <kuromike0629@gmail.com>
2017-12-13 15:29:18 +0900

    [add]require open3

--- lib/tomoyo_linux_ruby.rb
+++ lib/tomoyo_linux_ruby.rb
@@ -1,5 +1,9 @@
 require "tomoyo_linux_ruby/version"
+require "tomoyo_linux_ruby/tomoyo_policy"
+require "tomoyo_linux_ruby/tomoyo_exceptions"
+require "tomoyo_linux_ruby/tomoyo_domain"
+require "open3"
 
 module TomoyoLinuxRuby
-  # Your code goes here...
+
 end
